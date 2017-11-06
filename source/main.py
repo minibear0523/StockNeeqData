0ba2698407742119d432b61364339fd3fdb6152a
minibear <lei.zhang.bupt08@gmail.com>
2017-11-06 09:44:44 +0800

    MOD:

--- source/main.py
+++ source/main.py
@@ -3,19 +3,21 @@ from fetcher import Fetcher
 from parser import Parser
 from report import Report
 from db import DB
+import arrow
 
 
 def begin():
-    # f = Fetcher()
-    # fetch_result = f.fetch()
+    f = Fetcher()
+    fetch_result = f.fetch()
     p = Parser()
     parse_result = p.parse()
 
     db = DB()
     db.insert(parse_result)
 
-    r = Report(data=parse_result)
-    r.send_report()
+    if arrow.now().format('dddd') == 'Friday':
+        r = Report(data=parse_result)
+        r.send_report()
 
 if __name__ == '__main__':
     begin()
