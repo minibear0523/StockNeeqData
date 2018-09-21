d35592f7f69e0a52d5e70fea6229cb15823b4cc0
LeiZhang <lei.zhang.bupt08@gmail.com>
2018-09-21 13:04:35 +0800

    <x>

--- source/report.py
+++ source/report.py
@@ -48,7 +48,7 @@ class Report(object):
             self.report += '<br>'
             self.report += '全国新三板挂牌公司%s家, 其中天津%s家' % (total_data.loc['新三板', '本周总量'], total_data.loc['新三板', '本周天津地区'])
             self.report += '<br>'
-            self.report += '截止到2018年7月底,天津市科委评定的规模超万亿元科技型企业共计4,386家'
+            self.report += '截止到2018年8月底,天津市科委评定的规模超万亿元科技型企业共计4,397家'
 
             self.subject = '数据周报: %s至%s' % (last_date, self.date.format('YYYY-MM-DD'))
         else:
